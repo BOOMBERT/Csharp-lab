ffba3813414d14fb8304a0466d1a5d4c5377f693
BOOMBERT <norbert.domagala2005@gmail.com>
2024-06-23 15:51:35 +0200

    BattleshipGame: Added column and row headers.

--- BattleshipGame/Board.cs
+++ BattleshipGame/Board.cs
@@ -32,9 +32,21 @@ namespace BattleshipGame
         private string Display()
         {
             var boardStringBuilder = new StringBuilder();
+            boardStringBuilder.Append(new string(' ', Size.ToString().Length + 1));
 
             for (int i = 0; i < Size; i++)
             {
+                char columnHeader = (char)('A' + (i % 26));
+                boardStringBuilder.Append(columnHeader).Append(' ');
+            }
+            boardStringBuilder.Append(Environment.NewLine);
+
+            for (int i = 0; i < Size; i++)
+            {
+                int rowHeader = i + 1;
+                int initialSpaces = Size.ToString().Length - rowHeader.ToString().Length;
+                boardStringBuilder.Append(new string(' ', initialSpaces)).Append(rowHeader).Append(' ');
+
                 for (int j = 0; j < Size; j++)
                 {
                     boardStringBuilder.Append(board[i, j]).Append(' ');
